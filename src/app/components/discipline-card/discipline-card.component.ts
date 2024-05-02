adff4a046abac30158128bccfac3332938fad747
Brenorml <breno.lucena@gmail.com>
2024-05-02 22:06:54 +0100

    completed index-card started discipline-card

--- src/app/components/discipline-card/discipline-card.component.ts
+++ src/app/components/discipline-card/discipline-card.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-discipline-card',
   templateUrl: './discipline-card.component.html',
-  styleUrls: ['./discipline-card.component.css']
+  styleUrls: ['./discipline-card.component.css', './discipline-card.component.recursive.css']
 })
 export class DisciplineCardComponent implements OnInit {
+
+  @Input()
+  moduleTitle: string = "";
+  @Input()
+  teacher: string = "";
+  @Input()
+  goals: string = "";
+  @Input()
+  activities: string = "";
+  @Input()
+  reflection: string = "";
+  @Input()
+  homeworkUrl: string = "";
+  @Input()
+  homeworkImg: string = "";
+  
 
   constructor() { }
 
